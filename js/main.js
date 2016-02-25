3671485d9a98a79a126613280da742608ee0deb4
sson4 <sson4@masonlive.gmu.edu>
2016-02-25 14:11:55 -0500

    Game is completed

--- js/main.js
+++ js/main.js
@@ -1,52 +1,140 @@
-window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/master/resources/Project%20Templates/Basic
+
+
+
+var game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update});
+
+var player;
+var lasers;
+
+var fireRate = 1;
+var nextFire = 0;
+var startBug = 1;
+var bugCount = startBug;
+
+
+function preload() {
+    game.load.image('background', 'assets/image/background.jpg');
+    game.load.image('bee', 'assets/image/bee.png');
+    game.load.image('ladybug', 'assets/image/ladybug.png');
+    game.load.image('mosquito', 'assets/image/mosquito.png');
+    game.load.image('bugspray','assets/image/bugspray.png');
+    game.load.image('laser','assets/image/laser.png');
+}
+
+
+
+function create() {
+   
+    game.physics.startSystem(Phaser.Physics.ARCADE);
     
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
+    game.add.sprite(0,0, 'background');
     
-    "use strict";
+    lasers = game.add.group();
+    lasers.enableBody = true;
+    lasers.physicsBodyType = Phaser.Physics.ARCADE;
+    lasers.createMultiple(1, 'laser' );
+    lasers.setAll('checkWorldBounds', true);
+    lasers.setAll('outOfBoundsKill', true);
     
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game', { preload: preload, create: create, update: update } );
     
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+    
+    
+    bugs = game.add.group();
+    bugs.enableBody = true;
+    bugs.physicsBodyType = Phaser.Physics.ARCADE;
+    for(var i = 0; i < startBug; i++){
+        var x = Math.round(Math.random()*game.world.width);
+        var y = Math.round(Math.random()*game.world.height);
+        var mosquito = bugs.create(x,0,'mosquito');
+        mosquito.body.velocity.x = x/5;
+        mosquito.body.velocity.y = y/5;
     }
     
-    var bouncy;
+
+  
     
-    function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
+    player = game.add.sprite(game.world.width*0.5, game.world.height*0.5, 'bugspray');
+    player.scale.setTo(-0.2);
+    player.anchor.setTo(0.8,0.2);
+    game.physics.enable(player);
+    player.body.allowRotation = false;
+  
+}
+
+function update() {
+    
+    player.rotation = game.physics.arcade.angleToPointer(player);
+    game.physics.arcade.overlap(lasers, bugs, killBugs, null, this);
+    game.physics.arcade.overlap(bugs, player, playerDied, null, this);
+    
+    
+    if (game.input.activePointer.isDown)
+    {
+        fire();
+    }
+    bugs.forEachExists(checkBound);
+    if (bugCount==0){
+        startBug++;
+        bugCount = startBug;
+        for(var i = 0; i < startBug; i++){
+            var x = Math.round(Math.random()*game.world.width);
+            var y = Math.round(Math.random()*game.world.height);
+            var mosquito = bugs.create(x,0,'mosquito');
+            mosquito.body.velocity.x = x/5;
+            mosquito.body.velocity.y = y/5;
+        }
+    }
+
+}
+
+function fire() {
+
+    if (game.time.now > nextFire && lasers.countDead() > 0)
+    {
+        nextFire = game.time.now + fireRate;
+
+        var laser = lasers.getFirstDead();
+
+        laser.reset(player.x , player.y );
+
+        game.physics.arcade.moveToPointer(laser, 700);
+    }
+
+}
+
+
+function checkBound (sprite) {
+    if (sprite.x < 0) 
+    {
+        sprite.x = game.width;
+    } 
+    else if (sprite.x > game.width) 
+    {
+        sprite.x = 0;
+    } 
+
+    if (sprite.y < 0) 
+    {
+        sprite.y = game.height;
         
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something awesome.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+    } 
+    else if (sprite.y > game.height) 
+    {
+        sprite.y = 0;
     }
+}
+
+
+function killBugs(lasers, bugs){
     
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, this.game.input.activePointer, 500, 500, 500 );
-    }
-};
+    bugs.kill();
+    bugCount--;
+    
+}
+
+function playerDied(player, bugs){
+    
+    player.kill();
+    lasers.kill();
+    gameOverText.text = 'Game Over';
+}
